1ed9cfd70c81e12549a8748ff3777bcdd17f984a
hhoutmann@gmail.com <hhoutmann@gmail.com>
2018-02-12 19:19:53 +0100

    implentation de test

--- irontask_back/Tests/TestsAPI/tests_BenevoleModel.py
+++ irontask_back/Tests/TestsAPI/tests_BenevoleModel.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from ..models import Benevole
+from irontask_back.models import Benevole
 from django.test import Client
 
 
@@ -7,25 +7,11 @@ class test_BenevoleCase(TestCase):
     c = Client()
 
     def setUp(self):
-        Benevole.objects.create(nom='HOUTMANN', prenom='hadrien', sexe = 'F'
-                                ,adresse="coucou", codePostal='88200', ville='remiremont',
-                                telephoneFixe = '0601403635', telephonePortable = '0601403635')
+        Benevole.objects.create(nom='HOUTMANN', prenom='hadrien', sexe='F'
+                                , adresse="coucou", codePostal='88200', ville='remiremont',
+                                telephoneFixe='0601403635', telephonePortable='0601403635')
         self.b = Benevole.objects.get(nom='HOUTMANN')
 
-
-    def test_Attribut(self):
-        self.assertEqual(self.b.nom, 'HOUTMANN')
-        self.assertEqual(self.b.adresse, 'coucou')
-        self.assertEqual(self.b.prenom, 'hadrien')
-        self.assertEqual(self.b.codePostal, '88200')
-        self.assertEqual(self.b.ville, 'remiremont')
-        self.assertEqual(self.b.telephoneFixe, '0601403635')
-        self.assertEqual(self.b.telephonePortable, '0601403635')
-
-
-    def __str__(self):
-        b = Benevole.objects.get(nom='HOUTMANN')
-        self.assertEqual(b.__str__(), 'HOUTMANN hadrien')
 
 
     def test_GetBenevoleIdReponse(self):
@@ -41,7 +27,6 @@ class test_BenevoleCase(TestCase):
                          b'"telephonePortable":"0601403635"}')
 
     def test_ApiReponse(self):
-
         r = self.c.get('/benevole/')
         self.assertEqual(r.status_code, 200)
 
@@ -52,11 +37,3 @@ class test_BenevoleCase(TestCase):
                          b'"sexe":"F","adresse":"coucou","codePostal":"88200",'
                          b'"ville":"remiremont","telephoneFixe":"0601403635",'
                          b'"telephonePortable":"0601403635"}]')
-
-
-
-
-
-
-
-
---
+++ irontask_back/Tests/TestsModels/TestsBenevole.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from ...models import Benevole
+from django.test import Client
+
+
+class test_BenevoleCase(TestCase):
+    c = Client()
+
+    def setUp(self):
+        Benevole.objects.create(nom='HOUTMANN', prenom='hadrien', sexe='F'
+                                , adresse="coucou", codePostal='88200', ville='remiremont',
+                                telephoneFixe='0601403635', telephonePortable='0601403635')
+        self.b = Benevole.objects.get(nom='HOUTMANN')
+
+
+    def test__str__(self):
+        b = Benevole.objects.get(nom='HOUTMANN')
+        self.assertEqual(b.__str__(), 'HOUTMANN hadrien')
+
+    def test_Attribut(self):
+        self.assertEqual(self.b.nom, 'HOUTMANN')
+        self.assertEqual(self.b.adresse, 'coucou')
+        self.assertEqual(self.b.prenom, 'hadrien')
+        self.assertEqual(self.b.codePostal, '88200')
+        self.assertEqual(self.b.ville, 'remiremont')
+        self.assertEqual(self.b.telephoneFixe, '0601403635')
+        self.assertEqual(self.b.telephonePortable, '0601403635')
